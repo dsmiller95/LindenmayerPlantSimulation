92747e97beae109a92a20f29dbf85521fba2c7ff
Dan Miller <dsmiller95@gmail.com>
2021-03-27 18:07:37 -0500

    add mesh variant options, which inheret all the same settings and symbols. and allows the L-system to index the variants inside the same symbol by a parameter. increment version foor next release at some point

--- Packages/com.dman.l-system/Runtime/UnityObjects/TurtleMeshOperations.cs
+++ Packages/com.dman.l-system/Runtime/UnityObjects/TurtleMeshOperations.cs
@@ -1,6 +1,7 @@
 using Dman.MeshDraftExtensions;
 using ProceduralToolkit;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Dman.LSystem.UnityObjects
@@ -10,8 +11,11 @@ namespace Dman.LSystem.UnityObjects
     {
         public char Character;
         public Mesh MeshRef;
+        [Tooltip("a list of variants available. If populated, indexed by the first parameter of the symbol")]
+        public Mesh[] MeshVariants;
         public Vector3 IndividualScale;
 
+        [Tooltip("Whether or not to scale the mesh based on an input parameter. Will accept the first parameter, unless mesh variants are used. In which case it will use the second parameter.")]
         public bool ParameterScale;
         public Vector3 ScalePerParameter;
 
@@ -28,18 +32,22 @@ namespace Dman.LSystem.UnityObjects
         {
             foreach (var meshKey in meshKeys)
             {
-                var newDraft = new MeshDraft(meshKey.MeshRef);
-                var bounds = meshKey.MeshRef.bounds;
-                newDraft.Move(Vector3.right * (-bounds.center.x + bounds.size.x / 2));
-                newDraft.Scale(meshKey.IndividualScale);
+                var meshes = new[] { meshKey.MeshRef }.Concat(meshKey.MeshVariants)
+                    .Select(mesh =>
+                    {
+                        var newDraft = new MeshDraft(mesh);
+                        var bounds = mesh.bounds;
+                        newDraft.Move(Vector3.right * (-bounds.center.x + bounds.size.x / 2));
+                        newDraft.Scale(meshKey.IndividualScale);
 
-                var transformPostMesh = meshKey.AlsoMove ?
-                      Matrix4x4.Translate(new Vector3(bounds.size.x * meshKey.IndividualScale.x, 0, 0))
-                    : Matrix4x4.identity;
+                        var transformPostMesh = meshKey.AlsoMove ?
+                              Matrix4x4.Translate(new Vector3(bounds.size.x * meshKey.IndividualScale.x, 0, 0))
+                            : Matrix4x4.identity;
+                        return (newDraft, transformPostMesh);
+                    });
                 yield return new TurtleMeshOperator(
                     meshKey.Character,
-                    transformPostMesh,
-                    newDraft,
+                    meshes.ToArray(),
                     meshKey.ParameterScale,
                     meshKey.ScalePerParameter,
                     meshKey.UseThickness);
@@ -48,23 +56,20 @@ namespace Dman.LSystem.UnityObjects
 
         class TurtleMeshOperator : ITurtleOperator<TurtleState>
         {
-            private MeshDraft generatedMesh;
-            private Matrix4x4 transformPostMesh;
+            private (MeshDraft, Matrix4x4)[] generatedMeshes;
             private bool scaling;
             private Vector3 scalePerParameter;
             private bool thickness;
             public char TargetSymbol { get; private set; }
             public TurtleMeshOperator(
                 char symbol,
-                Matrix4x4 transform,
-                MeshDraft generatedMesh,
+                (MeshDraft, Matrix4x4)[] generatedMeshes,
                 bool scaling,
                 Vector3 scalePerParameter,
                 bool thickness)
             {
                 TargetSymbol = symbol;
-                transformPostMesh = transform;
-                this.generatedMesh = generatedMesh;
+                this.generatedMeshes = generatedMeshes;
                 this.scaling = scaling;
                 this.scalePerParameter = scalePerParameter;
                 this.thickness = thickness;
@@ -73,17 +78,28 @@ namespace Dman.LSystem.UnityObjects
             public TurtleState Operate(TurtleState initialState, double[] parameters, MeshDraft targetDraft)
             {
                 var meshScale = initialState.transformation;
-                if (scaling && parameters.Length >= 1)
+
+                var selectedMesh = generatedMeshes[0];
+                if (generatedMeshes.Length > 1 && parameters.Length > 0)
                 {
-                    meshScale *= Matrix4x4.Scale(scalePerParameter * (float)parameters[0]);
+                    var index = ((int)parameters[0]) % generatedMeshes.Length;
+                    selectedMesh = generatedMeshes[index];
+                }
+
+
+                var scaleIndex = generatedMeshes.Length <= 1 ? 0 : 1;
+                if (scaling && parameters.Length > scaleIndex)
+                {
+                    var scale = parameters[scaleIndex];
+                    meshScale *= Matrix4x4.Scale(scalePerParameter * (float)scale);
                 }
                 if (thickness)
                 {
                     meshScale *= Matrix4x4.Scale(new Vector3(1, initialState.thickness, initialState.thickness));
                 }
 
-                targetDraft.AddWithTransform(generatedMesh, meshScale);
-                initialState.transformation *= transformPostMesh;
+                targetDraft.AddWithTransform(selectedMesh.Item1, meshScale);
+                initialState.transformation *= selectedMesh.Item2;
                 return initialState;
             }
         }
