faf45544feb71f374ba25bd21cc0094e8b848e7d
Dan Miller <dsmiller95@gmail.com>
2021-07-15 18:53:47 -0500

    put rule match completable into a job

--- Packages/com.dman.l-system/Runtime/SystemRuntime/LSystemEvaluator/LSystemParameterSizeCountingCompletable.cs
+++ Packages/com.dman.l-system/Runtime/SystemRuntime/LSystemEvaluator/LSystemParameterSizeCountingCompletable.cs
@@ -21,7 +21,7 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
         public CustomRuleSymbols customSymbols;
 
         /////////////// things owned by this step /////////
-
+        public NativeArray<int> parameterTotalSum;
 
         /////////////// things transferred to the next step /////////
         public SymbolStringBranchingCache branchingCache;
@@ -29,7 +29,6 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
 
         private float[] globalParameters;
         private JobHandle paramModificationDependency;
-        private int parameterTotalSum;
 
 
         /////////////// l-system native data /////////
@@ -38,19 +37,7 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
 
         public JobHandle currentJobHandle { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="systemState"></param>
-        /// <param name="lSystemNativeData"></param>
-        /// <param name="globalParameters"></param>
-        /// <param name="maxMemoryRequirementsPerSymbol"></param>
-        /// <param name="branchOpenSymbol"></param>
-        /// <param name="branchCloseSymbol"></param>
-        /// <param name="includedCharactersByRuleIndex"></param>
-        /// <param name="customSymbols"></param>
-        /// <param name="parameterModificationJobDependency">A dependency on a job which only makes changes to the parameters of the source symbol string.
-        ///     the symbols themselves must be constant</param>
+
         public LSystemParameterSizeCountingCompletable(
             LSystemState<float> systemState,
             DependencyTracker<SystemLevelRuleNativeData> lSystemNativeData,
@@ -74,27 +61,20 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
             // 1.
             UnityEngine.Profiling.Profiler.BeginSample("Paramter counts");
             matchSingletonData = new NativeArray<LSystemSingleSymbolMatchData>(sourceSymbolString.Data.Length, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-            parameterTotalSum = 0;
-            var maxMemoryRequirementsPerSymbol = nativeData.Data.maxParameterMemoryRequirementsPerSymbol;
-            for (int i = 0; i < sourceSymbolString.Data.Length; i++)
+
+            parameterTotalSum = new NativeArray<int>(1, Allocator.TempJob);
+            var memorySizeJob = new SymbolStringMemoryRequirementsJob
             {
-                var symbol = sourceSymbolString.Data[i];
-                var matchData = new LSystemSingleSymbolMatchData()
-                {
-                    tmpParameterMemorySpace = JaggedIndexing.GetWithNoLength(parameterTotalSum)
-                };
-                if (maxMemoryRequirementsPerSymbol.TryGetValue(symbol, out var memoryRequirements))
-                {
-                    parameterTotalSum += memoryRequirements.maxParameters;
-                    matchData.isTrivial = false;
-                }
-                else
-                {
-                    matchData.isTrivial = true;
-                    matchData.tmpParameterMemorySpace.length = 0;
-                }
-                matchSingletonData[i] = matchData;
-            }
+                matchSingletonData = matchSingletonData,
+                memoryRequirementsPerSymbol = nativeData.Data.maxParameterMemoryRequirementsPerSymbol,
+                parameterTotalSum = parameterTotalSum,
+                sourceSymbolString = sourceSymbolString.Data
+            };
+
+            currentJobHandle = memorySizeJob.Schedule();
+            sourceSymbolString.RegisterDependencyOnData(currentJobHandle);
+            nativeData.RegisterDependencyOnData(currentJobHandle);
+
 
             UnityEngine.Profiling.Profiler.EndSample();
 
@@ -112,10 +92,12 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
         public ICompletable StepNext()
         {
             currentJobHandle.Complete();
+            var paramTotal = parameterTotalSum[0];
+            parameterTotalSum.Dispose();
 
             return new LSystemRuleMatchCompletable(
                 matchSingletonData,
-                parameterTotalSum,
+                paramTotal,
                 branchingCache,
                 systemState,
                 nativeData,
@@ -148,6 +130,7 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
             // TODO
             currentJobHandle.Complete();
             matchSingletonData.Dispose();
+            parameterTotalSum.Dispose();
             if (branchingCache.IsCreated) branchingCache.Dispose();
             return inputDeps;
         }
@@ -156,6 +139,7 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
         {
             currentJobHandle.Complete();
             matchSingletonData.Dispose();
+            parameterTotalSum.Dispose();
             if (branchingCache.IsCreated) branchingCache.Dispose();
         }
     }
@@ -165,95 +149,41 @@ namespace Dman.LSystem.SystemRuntime.LSystemEvaluator
     /// Step 2. match all rules which could possibly match, without checking the conditional expressions
     /// </summary>
     [BurstCompile]
-    public struct SymbolStringMemoryRequirementsJob : IJobParallelForBatch
+    public struct SymbolStringMemoryRequirementsJob : IJob
     {
+        public NativeArray<int> parameterTotalSum;
         public NativeArray<LSystemSingleSymbolMatchData> matchSingletonData;
 
         [ReadOnly]
-        [NativeDisableParallelForRestriction]
-        public SymbolString<float> sourceData;
-        [NativeDisableParallelForRestriction]
-        public NativeArray<float> tmpParameterMemory;
-
+        public NativeHashMap<int, MaxMatchMemoryRequirements> memoryRequirementsPerSymbol;
         [ReadOnly]
-        [NativeDisableParallelForRestriction]
-        public NativeArray<OperatorDefinition> globalOperatorData;
-        [ReadOnly]
-        [NativeDisableParallelForRestriction]
-        public NativeArray<RuleOutcome.Blittable> outcomes;
-        [ReadOnly]
-        [NativeDisableParallelForRestriction]
-        public NativeArray<float> globalParams;
+        public SymbolString<float> sourceSymbolString;
 
-        [ReadOnly]
-        public NativeOrderedMultiDictionary<BasicRule.Blittable> blittableRulesByTargetSymbol;
 
-        [ReadOnly]
-        public SymbolStringBranchingCache branchingCache;
-
-        public uint seed;
-
-        public void Execute(int startIndex, int batchSize)
+        public void Execute()
         {
-            var forwardsMatchHelperStack = new TmpNativeStack<SymbolStringBranchingCache.BranchEventData>(5);
-            var rnd = LSystemStepper.RandomFromIndexAndSeed(((uint)startIndex) + 1, seed);
-            for (int i = 0; i < batchSize; i++)
+            var totalParametersAlocated = 0;
+            for (int i = 0; i < sourceSymbolString.Length; i++)
             {
-                ExecuteAtIndex(i + startIndex, forwardsMatchHelperStack, ref rnd);
-            }
-        }
-        private void ExecuteAtIndex(
-            int indexInSymbols,
-            TmpNativeStack<SymbolStringBranchingCache.BranchEventData> helperStack,
-            ref Unity.Mathematics.Random random)
-        {
-            var matchSingleton = matchSingletonData[indexInSymbols];
-            if (matchSingleton.isTrivial)
-            {
-                // if match is trivial, then no parameters are captured. the rest of the algo will read directly from the source index
-                //  and no transformation will take place.
-                return;
-            }
-
-            var symbol = sourceData.symbols[indexInSymbols];
-
-            if (!blittableRulesByTargetSymbol.TryGetValue(symbol, out var ruleIndexing) || ruleIndexing.length <= 0)
-            {
-                matchSingleton.errorCode = LSystemMatchErrorCode.TRIVIAL_SYMBOL_NOT_INDICATED_AT_MATCH_TIME;
-                matchSingletonData[indexInSymbols] = matchSingleton;
-                return;
-            }
-
-            var anyRuleMatched = false;
-            var currentIndexInParameterMemory = matchSingleton.tmpParameterMemorySpace.index;
-            for (byte i = 0; i < ruleIndexing.length; i++)
-            {
-                var rule = blittableRulesByTargetSymbol[ruleIndexing, i];
-                var success = rule.PreMatchCapturedParametersWithoutConditional(
-                    branchingCache,
-                    sourceData,
-                    indexInSymbols,
-                    tmpParameterMemory,
-                    currentIndexInParameterMemory,
-                    ref matchSingleton,
-                    helperStack,
-                    globalParams,
-                    globalOperatorData,
-                    ref random,
-                    outcomes
-                    );
-                if (success)
+                var symbol = sourceSymbolString[i];
+                var matchData = new LSystemSingleSymbolMatchData()
                 {
-                    anyRuleMatched = true;
-                    matchSingleton.matchedRuleIndexInPossible = i;
-                    break;
+                    tmpParameterMemorySpace = JaggedIndexing.GetWithNoLength(totalParametersAlocated)
+                };
+                if (memoryRequirementsPerSymbol.TryGetValue(symbol, out var memoryRequirements))
+                {
+                    totalParametersAlocated += memoryRequirements.maxParameters;
+                    matchData.isTrivial = false;
                 }
+                else
+                {
+                    matchData.isTrivial = true;
+                    matchData.tmpParameterMemorySpace.length = 0;
+                }
+                matchSingletonData[i] = matchData;
             }
-            if (anyRuleMatched == false)
-            {
-                matchSingleton.isTrivial = true;
-            }
-            matchSingletonData[indexInSymbols] = matchSingleton;
+
+            parameterTotalSum[0] = totalParametersAlocated;
         }
     }
 }
