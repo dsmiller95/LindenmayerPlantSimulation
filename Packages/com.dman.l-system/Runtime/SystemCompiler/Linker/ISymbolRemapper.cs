529ce464643eee04be21ebedb08aafff7fbb797c
Dan Miller <dsmiller95@gmail.com>
2022-03-25 10:42:53 -0500

    add test to ensure organs get filtered out correctly, and other turtle features such as rotation and branching are supported

--- Packages/com.dman.l-system/Runtime/SystemCompiler/Linker/ISymbolRemapper.cs
+++ Packages/com.dman.l-system/Runtime/SystemCompiler/Linker/ISymbolRemapper.cs
@@ -16,45 +16,19 @@ namespace Dman.LSystem.SystemCompiler.Linker
         public char GetCharacterInRoot(int symbol);
     }
 
-    public class SimpleSymbolRemapper : ISymbolRemapper, IEnumerable<KeyValuePair<int, char>>
+    /// <summary>
+    /// maps the character to its character code directly
+    /// </summary>
+    public class SimpleSymbolRemapper : ISymbolRemapper
     {
-        private Dictionary<int, char> remapping = new Dictionary<int, char> ();
-
-        public void SetMapping(char character, int symbol)
-        {
-            remapping[symbol] = character;
-        }
-
-        public void Add(char character, int symbol)
-        {
-            this.SetMapping(character, symbol);
-        }
-
         public char GetCharacterInRoot(int symbol)
         {
-            return remapping[symbol];
+            return (char)symbol;
         }
 
         public int GetSymbolFromRoot(char character)
         {
-            foreach (var pair in remapping)
-            {
-                if (pair.Value == character)
-                {
-                    return pair.Key;
-                }
-            }
-            throw new KeyNotFoundException();
-        }
-
-        public IEnumerator<KeyValuePair<int, char>> GetEnumerator()
-        {
-            return ((IEnumerable<KeyValuePair<int, char>>)remapping).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return ((IEnumerable)remapping).GetEnumerator();
+            return character;
         }
     }
 }
--- Packages/com.dman.l-system/Runtime/Tests/EditMode/OrganPositioningTurtleInterpretorTests.cs
+++ Packages/com.dman.l-system/Runtime/Tests/EditMode/OrganPositioningTurtleInterpretorTests.cs
@@ -15,48 +15,73 @@ using UnityEngine.TestTools;
 
 public class OrganPositioningTurtleInterpretorTests
 {
-    [UnityTest]
-    public IEnumerator TurtleOrganIdsCompilesSingleOrgan() => UniTask.ToCoroutine(async () =>
+    private OrganPositioningTurtleInterpretor GetInterpretor(char[] meshKeys)
     {
         var meshOperations = ScriptableObject.CreateInstance<TurtleMeshOperations>();
-        var mesh = new MeshKey
+        meshOperations.meshKeys = meshKeys.Select(x =>
         {
-            Character = 'C',
-            MeshRef = Resources.GetBuiltinResource<Mesh>("Cube.fbx"),
-            MeshVariants = new MeshVariant[0],
-            material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Diffuse.mat"),
-            IndividualScale = Vector3.one,
-            ParameterScale = false,
-            ScaleIsAdditional = false,
-            VolumetricScale = false,
-            ScalePerParameter = Vector3.one,
-            AlsoMove = true,
-            UseThickness = false,
-            volumetricDurabilityValue = 0,
-        };
-        meshOperations.meshKeys = new[] { mesh };
+            return new MeshKey
+            {
+                Character = x,
+                MeshRef = Resources.GetBuiltinResource<Mesh>("Cube.fbx"),
+                MeshVariants = new MeshVariant[0],
+                material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Diffuse.mat"),
+                IndividualScale = Vector3.one,
+                ParameterScale = false,
+                ScaleIsAdditional = false,
+                VolumetricScale = false,
+                ScalePerParameter = Vector3.one,
+                AlsoMove = true,
+                UseThickness = false,
+                volumetricDurabilityValue = 0,
+            };
+        }).ToArray();
 
-        var opSets = new List<TurtleOperationSet>() { meshOperations };
+        var turnOperations = ScriptableObject.CreateInstance<TurtleRotateOperations>();
+        turnOperations.defaultRollTheta = 90;
+        turnOperations.defaultTurnTheta = 90;
+        turnOperations.defaultTiltTheta = 90;
+
+        var opSets = new List<TurtleOperationSet>() { meshOperations, turnOperations };
         var defaultState = new TurtleState
         {
             transformation = Matrix4x4.identity,
             thickness = 1,
             organIdentity = new UIntFloatColor32(0)
         };
-        var simpleRemapper = new SimpleSymbolRemapper()
-        {
-            {'[', 1 },
-            {']', 2 },
-            {'C', 'C' },
-        };
+        var simpleRemapper = new SimpleSymbolRemapper();
+
         var customSymbols = new CustomRuleSymbols
         {
-            branchOpenSymbol = 1,
-            branchCloseSymbol = 2,
+            branchOpenSymbol = '[',
+            branchCloseSymbol = ']',
         };
 
+        return new OrganPositioningTurtleInterpretor(opSets, defaultState, simpleRemapper, customSymbols);
+    }
+
+
+    private void ExpectPositions(List<TurtleOrganInstance> organs, List<Vector3> positions)
+    {
+        Assert.AreEqual(positions.Count, organs.Count);
+        for (int instanceNum = 0; instanceNum < organs.Count; instanceNum++)
+        {
+            var instance = organs[instanceNum];
+            var position = ((Matrix4x4)instance.organTransform).MultiplyPoint(Vector3.zero);
+            // assert position and orientation
+            var expectedPos = positions[instanceNum];
+
+            Assert.AreEqual(expectedPos.x, position.x, 1e-5, $"Expected organ at {expectedPos}, but was at {position}. index {instanceNum}");
+            Assert.AreEqual(expectedPos.y, position.y, 1e-5, $"Expected organ at {expectedPos}, but was at {position}. index {instanceNum}");
+            Assert.AreEqual(expectedPos.z, position.z, 1e-5, $"Expected organ at {expectedPos}, but was at {position}. index {instanceNum}");
+        }
+    }
+
+    [UnityTest]
+    public IEnumerator TurtleOrganIdsCompilesSingleOrgan() => UniTask.ToCoroutine(async () =>
+    {
         List<TurtleOrganInstance> organInstances;
-        using (var turtle = new OrganPositioningTurtleInterpretor(opSets, defaultState, simpleRemapper, customSymbols))
+        using (var turtle = GetInterpretor(new[] { 'C' }))
         using (var systemState = new DefaultLSystemState("C"))
         using (var cancellation = new CancellationTokenSource())
         {
@@ -64,55 +89,18 @@ public class OrganPositioningTurtleInterpretorTests
             organInstances = turtle.FilterOrgansByCharacter(meshInstances, 'C').ToList();
         }
 
-        Assert.AreEqual(1, organInstances.Count);
-        var instance = organInstances[0];
-        // assert position and orientation
-        Assert.AreEqual(Vector3.zero, ((Matrix4x4)instance.organTransform).MultiplyPoint(Vector3.zero));
-        Assert.AreEqual(Vector3.one, ((Matrix4x4)instance.organTransform).MultiplyPoint(Vector3.one));
+        var expected = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+        };
+        ExpectPositions(organInstances, expected);
     });
 
     [UnityTest]
     public IEnumerator TurtleOrganCompilesMultiOrgans() => UniTask.ToCoroutine(async () =>
     {
-        var meshOperations = ScriptableObject.CreateInstance<TurtleMeshOperations>();
-        var mesh = new MeshKey
-        {
-            Character = 'C',
-            MeshRef = Resources.GetBuiltinResource<Mesh>("Cube.fbx"),
-            MeshVariants = new MeshVariant[0],
-            material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Diffuse.mat"),
-            IndividualScale = Vector3.one,
-            ParameterScale = false,
-            ScaleIsAdditional = false,
-            VolumetricScale = false,
-            ScalePerParameter = Vector3.one,
-            AlsoMove = true,
-            UseThickness = false,
-            volumetricDurabilityValue = 0,
-        };
-        meshOperations.meshKeys = new[] { mesh };
-
-        var opSets = new List<TurtleOperationSet>() { meshOperations };
-        var defaultState = new TurtleState
-        {
-            transformation = Matrix4x4.identity,
-            thickness = 1,
-            organIdentity = new UIntFloatColor32(0)
-        };
-        var simpleRemapper = new SimpleSymbolRemapper()
-        {
-            {'[', 1 },
-            {']', 2 },
-            {'C', 'C' },
-        };
-        var customSymbols = new CustomRuleSymbols
-        {
-            branchOpenSymbol = 1,
-            branchCloseSymbol = 2,
-        };
-
         List<TurtleOrganInstance> organInstances;
-        using (var turtle = new OrganPositioningTurtleInterpretor(opSets, defaultState, simpleRemapper, customSymbols))
+        using (var turtle = GetInterpretor(new[] { 'C' }))
         using (var systemState = new DefaultLSystemState("CCCCC"))
         using (var cancellation = new CancellationTokenSource())
         {
@@ -120,14 +108,86 @@ public class OrganPositioningTurtleInterpretorTests
             organInstances = turtle.FilterOrgansByCharacter(meshInstances, 'C').ToList();
         }
 
-        Assert.AreEqual(5, organInstances.Count);
-        for (int instanceNum = 0; instanceNum < organInstances.Count; instanceNum++)
+        var expected = new List<Vector3>
         {
-            var instance = organInstances[instanceNum];
-            var position = ((Matrix4x4)instance.organTransform).MultiplyPoint(Vector3.zero);
-            // assert position and orientation
-            var expectedPos = new Vector3(1, 0, 0) * instanceNum;
-            Assert.AreEqual(expectedPos, position, $"Expected organ at {expectedPos}, but was at {position}. index {instanceNum}");
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(2, 0, 0),
+            new Vector3(3, 0, 0),
+            new Vector3(4, 0, 0),
+        };
+        ExpectPositions(organInstances, expected);
+    });
+
+    [UnityTest]
+    public IEnumerator TurtleOrganCompilesMultiOrganTypes() => UniTask.ToCoroutine(async () =>
+    {
+        List<TurtleOrganInstance> organInstances;
+        using (var turtle = GetInterpretor(new[] { 'C', 'D' }))
+        using (var systemState = new DefaultLSystemState("CDCDDCDCDCDDDC"))
+        using (var cancellation = new CancellationTokenSource())
+        {
+            var meshInstances = await turtle.CompileStringToTransformsWithMeshIds(systemState.currentSymbols, Matrix4x4.identity, cancellation.Token);
+            organInstances = turtle.FilterOrgansByCharacter(meshInstances, 'C').ToList();
         }
+
+        var expected = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(2, 0, 0),
+            new Vector3(5, 0, 0),
+            new Vector3(7, 0, 0),
+            new Vector3(9, 0, 0),
+            new Vector3(13, 0, 0),
+        };
+        ExpectPositions(organInstances, expected);
+    });
+
+    [UnityTest]
+    public IEnumerator TurtleOrganCompilesWithBending() => UniTask.ToCoroutine(async () =>
+    {
+        List<TurtleOrganInstance> organInstances;
+        using (var turtle = GetInterpretor(new[] { 'C' }))
+        using (var systemState = new DefaultLSystemState("C+C-C+C-C"))
+        using (var cancellation = new CancellationTokenSource())
+        {
+            var meshInstances = await turtle.CompileStringToTransformsWithMeshIds(systemState.currentSymbols, Matrix4x4.identity, cancellation.Token);
+            organInstances = turtle.FilterOrgansByCharacter(meshInstances, 'C').ToList();
+        }
+
+        var expected = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 1),
+            new Vector3(2, 0, 1),
+            new Vector3(2, 0, 2),
+        };
+        ExpectPositions(organInstances, expected);
+    });
+
+    [UnityTest]
+    public IEnumerator TurtleOrganCompilesWithBendingAndBranching() => UniTask.ToCoroutine(async () =>
+    {
+        List<TurtleOrganInstance> organInstances;
+        using (var turtle = GetInterpretor(new[] { 'C' }))
+        using (var systemState = new DefaultLSystemState("C[+CCC][-CCC]"))
+        using (var cancellation = new CancellationTokenSource())
+        {
+            var meshInstances = await turtle.CompileStringToTransformsWithMeshIds(systemState.currentSymbols, Matrix4x4.identity, cancellation.Token);
+            organInstances = turtle.FilterOrgansByCharacter(meshInstances, 'C').ToList();
+        }
+
+        var expected = new List<Vector3>
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 1),
+            new Vector3(1, 0, 2),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0,-1),
+            new Vector3(1, 0,-2),
+        };
+        ExpectPositions(organInstances, expected);
     });
 }
