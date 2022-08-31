dc5f359ea1551f8e0e9542b23c0b89a487af8352
Dan Miller <dsmiller95@gmail.com>
2022-08-31 16:53:13 -0500

    descronkled

--- Packages/com.dman.l-system/Runtime/UnityObjects/TurtleOperations/TurtleStemTrunkComposite/TurtleStemBuildingJob.cs
+++ Packages/com.dman.l-system/Runtime/UnityObjects/TurtleOperations/TurtleStemTrunkComposite/TurtleStemBuildingJob.cs
@@ -67,23 +67,57 @@ namespace Dman.LSystem.UnityObjects.StemTrunk
             var parentStemMeshMemory = organMeshAllocations[stemInstance.parentIndex + meshMemoryOffset];
             // create the rectangle strip. only supported when equal radial vertex count and same submesh
             var parentVertexOffset = submeshData.indexInVertexes + parentStemMeshMemory.vertexMemorySpace.index;
+
+            var myCircleIndexOffset = (GetNormalizedCircleOffset(parentStem.orientation, pointTransform) + 1) * stemInstance.radialResolution;
+
+
             for (int rectIndex = 0; rectIndex < stemInstance.radialResolution; rectIndex++)
             {
                 var nextIndex = (rectIndex + 1) % stemInstance.radialResolution;
                 var p1 = (uint)(rectIndex + parentVertexOffset);
                 var p2 = (uint)(nextIndex + parentVertexOffset);
-                var c1 = (uint)(rectIndex + vertexOffset);
-                var c2 = (uint)(nextIndex + vertexOffset);
+
+                // offset child indexes by index offset, determined by angular difference with the parent
+                var childIndex = (rectIndex + myCircleIndexOffset) % stemInstance.radialResolution;
+                var childNextIndex = (rectIndex + myCircleIndexOffset + 1) % stemInstance.radialResolution;
+                var c1 = (uint)(childIndex + vertexOffset);
+                var c2 = (uint)(childNextIndex + vertexOffset);
 
                 var triangleBase = rectIndex * 6 + triangleOffset;
                 triangleIndexes[triangleBase + 0] = p1;
-                triangleIndexes[triangleBase + 1] = c2;
-                triangleIndexes[triangleBase + 2] = c1;
+                triangleIndexes[triangleBase + 1] = c1;
+                triangleIndexes[triangleBase + 2] = c2;
 
                 triangleIndexes[triangleBase + 3] = p1;
-                triangleIndexes[triangleBase + 4] = p2;
-                triangleIndexes[triangleBase + 5] = c2;
+                triangleIndexes[triangleBase + 4] = c2;
+                triangleIndexes[triangleBase + 5] = p2;
             }
+        }
+
+        /// <summary>
+        /// returns a value representing the rotation required to align the y axis of <paramref name="next"/> up as closely as posslbe to the y axis of <paramref name="parent"/>
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="next"></param>
+        /// <returns>a value between -0.5 and 0.5, representing rotations about the x-axis from -180 to 180 degrees</returns>
+        private float GetNormalizedCircleOffset(Matrix4x4 parent, Matrix4x4 next)
+        {
+            var parentBasisPlaneX = parent.MultiplyVector(Vector3.forward);
+            var parentBasisPlaneY = parent.MultiplyVector(Vector3.up);
+            var parentBasisPlaneNormal = parent.MultiplyVector(Vector3.right);
+
+            var nextY = next.MultiplyVector(Vector3.up);
+            var nextYProjectedOnParentBasisPlane = ProjectOntoPlane(nextY, parentBasisPlaneX, parentBasisPlaneY);
+
+            var angleOffset = Vector3.SignedAngle(parentBasisPlaneY, nextYProjectedOnParentBasisPlane, parentBasisPlaneNormal);
+            return angleOffset / 360f;
+        }
+
+        private Vector3 ProjectOntoPlane(Vector3 projectionVector, Vector3 planeBasisX, Vector3 planeBasisY)
+        {
+            var projectedX = planeBasisX * (Vector3.Dot(planeBasisX, projectionVector));
+            var projectedY = planeBasisY * (Vector3.Dot(planeBasisY, projectionVector));
+            return projectedX + projectedY;
         }
 
         private Color32 ColorFromIdentity(UIntFloatColor32 identity, uint index)
