65d42bc2964f6031c8a61bd86ed181ba2c0706da
rasmus <1848463+rasmus-z@users.noreply.github.com>
2022-11-07 16:44:29 +0100

    Added a static class to ensure culture-independent number format when (de)serializing.

---
+++ Packages/com.dman.l-system/Runtime/NumberFormatHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Threading;
+using UnityEngine;
+
+namespace Dman.LSystem
+{
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoad]
+#endif
+    public static class NumberFormatHelper
+    {
+        static NumberFormatHelper()
+        {
+            InitializeInvariantNumberFormat();
+        }
+
+        /// <summary>
+        /// Sets the NumberFormat of the CurrentThreads CurrentCulture to Invariant, for when deserializing l-systems.
+        /// </summary>
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        public static void InitializeInvariantNumberFormat()
+        {
+            var currentThreadsCultureInfoClone = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+            currentThreadsCultureInfoClone.NumberFormat = NumberFormatInfo.InvariantInfo;
+            System.Threading.Thread.CurrentThread.CurrentCulture = currentThreadsCultureInfoClone;
+
+            #if DEBUG
+            Debug.Log($"NumberFormatHelper: CurrentCultures NumberFormat set to Invariant.");
+            #endif
+        }
+    }
+}
