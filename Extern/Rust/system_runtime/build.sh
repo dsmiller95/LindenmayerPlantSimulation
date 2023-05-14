450360d178062d8392eec13ecb2bb940f6c99dde
Dan Miller <dsmiller95@gmail.com>
2023-05-14 08:58:32 -0500

    update gitignore. don't need to commit the Lib folder or .meta at all.

--- Extern/Rust/system_runtime/build.sh
+++ Extern/Rust/system_runtime/build.sh
@@ -5,5 +5,7 @@ cargo build
 rm -f ../../../Packages/com.dman.l-system/External/Lib/*.dll
 
 cp ../../dotnet/* ../../../Packages/com.dman.l-system/External
+
+mkdir -p ../../../Packages/com.dman.l-system/External/Lib
 cp target/debug/system_runtime_rustlib.* ../../../Packages/com.dman.l-system/External/Lib
 
